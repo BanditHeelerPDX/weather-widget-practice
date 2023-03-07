9672ad96fb65fa06548d5c9e67751decba565a5f
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-03-06 21:44:26 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const countryEl = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour;
+    const minutes = time.getMinutes();
+    const ampm = hour >= 12 ? 'PM' : 'AM'
+
+    timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + ' ' + `<span id="am-pm">${ampm}</span>`
+
+    dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month];
+
+}, 1000);
+
+getWeatherData()
+
+function getWeatherData () {
+  // if ('geolocation' in navigator) {
+   
+    navigator.geolocation.getCurrentPosition((success) => {
+       
+        let {latitude, longitude} = success.coords;
+    
+
+        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${API_KEY}`).then(res => res.json()).then(data => {
+            console.log(data)
+        })
+    })
+// } else {
+    
+ }
+
+
+
+
+
+
+//  796d4a60796f715c7b650eb7c5eade88
